aa49b174f5c1da677414b20d96b21a2913fcd94e
motoMarkHsu <68104760+motoMarkHsu@users.noreply.github.com>
2020-07-14 16:17:34 +0800

    Add files via upload

--- ESP8266.ts
+++ ESP8266.ts
@@ -102,14 +102,14 @@ namespace MotoduinoWiFi {
         //let IFTTTCommand = "POST /trigger/"+ eventName+ "/with/key/"+ apikey+" HTTP/1.1\r\nHost: maker.ifttt.com\r\nUser-Agent: Arduino\r\nAccept: */*\r\nContent-Length: \r\n"+ nCommandLen+"\r\nContent-Type: application/json\r\nConnection: close\r\n{\"value1\":\""+ v1+"\",\"value2\":\""+ v2+"\",\"value3\":\""+ v3+"\"}"
         //let ATCommand = "AT+CIPSEND=" + (IFTTTCommand.length + 2)
 		
-        //let IFTTTCommand2 = "GET /trigger/"+ eventName+ "/with/key/"+ apikey+ "?value1="+ v1+ "&value2="+ v2+"&value3="+ v3+ " HTTP/1.1\r\nHost: maker.ifttt.com\r\nConnection: close\r\n\r\n\r\n\r\n"
-        let IFTTTCommand3 = "GET /trigger/"+ eventName+ "/with/key/"+ apikey+" HTTP/1.1\r\nHost: maker.ifttt.com\r\nConnection: close\r\n\r\n\r\n\r\n"
+        let IFTTTCommand2 = "GET /trigger/"+ eventName+ "/with/key/"+ apikey+ "?value1="+ v1+ "&value2="+ v2+"&value3="+ v3+ " HTTP/1.1\r\nHost: maker.ifttt.com\r\nConnection: close\r\n\r\n\r\n\r\n"
+        //let IFTTTCommand3 = "GET /trigger/"+ eventName+ "/with/key/"+ apikey+" HTTP/1.1\r\nHost: maker.ifttt.com\r\nConnection: close\r\n\r\n\r\n\r\n"
         //let IFTTTCommand4="GET /trigger/IFTTT_Trigger/with/key/d2tCByRKOVoOzbHT-PAelo?value1=1&value2=2&value3=3"
-        let ATCommand = "AT+CIPSEND=" + (IFTTTCommand3.length + 2)
+        let ATCommand = "AT+CIPSEND=" + (IFTTTCommand2.length + 2)
 		
         sendAT("AT+CIPSTART=\"TCP\",\"maker.ifttt.com\",80", 3000)
         sendAT(ATCommand)
-        sendAT(IFTTTCommand3,3000)
+        sendAT(IFTTTCommand2,3000)
         sendAT("AT+CIPCLOSE")
     }
 }
