ca150cee7545e72d32cc3667f9707e1a572825ef
motoMarkHsu <68104760+motoMarkHsu@users.noreply.github.com>
2020-07-14 08:25:17 +0800

    Add files via upload

--- ESP8266.ts
+++ ESP8266.ts
@@ -8,7 +8,7 @@ namespace MotoduinoWiFi {
     let bThingSpeak_Connected: boolean = false
 
     // write AT command with CR+LF ending
-    function sendAT(command: string, wait: number = 100) {
+    function sendAT(command: string, wait: number = 1000) {
         serial.writeString(command + "\u000D\u000A")
         basic.pause(wait)
     }
@@ -46,13 +46,14 @@ namespace MotoduinoWiFi {
     //% block="Motoduino WIFI Set| RX (Tx of micro:bit) %txd| TX (Rx of micro:bit) %rxd| Baud rate %baudrate| SSID %ssid| PASSWORD %passwd"
 
     export function Wifi_Setup(txd: SerialPin, rxd: SerialPin, baudrate: BaudRate, ssid: string, passwd: string): void {
-        serial.redirect(
+        /**
+		serial.redirect(
             txd,   //TX
             rxd,  //RX
-            baudrate
-        );
+            BaudRate.BaudRate9600
+        )
 
-        sendAT("AT+RESTORE", 1000) // restore to factory settings
+		sendAT("AT+RESTORE", 1000) // restore to factory settings
         sendAT("AT+CWMODE=1") // set to STA mode
         sendAT("AT+RST", 1000) // reset
         basic.pause(100)
@@ -62,6 +63,20 @@ namespace MotoduinoWiFi {
         sendAT("AT+CWJAP=\"" + ssid + "\",\"" + passwd + "\"", 0) // connect to Wifi router
         bAP_Connected = waitResponse()
         basic.pause(100)
+		**/
+		
+		serial.redirect(SerialPin.P13,SerialPin.P14,BaudRate.BaudRate9600)
+		bAP_Connected = false
+        bThingSpeak_Connected = false
+		
+		serial.writeString("AT+RST" + "\u000D" + "\u000A")
+    	basic.pause(1000)
+    	serial.writeString("AT+CWMODE_CUR=1" + "\u000D" + "\u000A")
+    	basic.pause(1000)
+    	let printT = "AT+CWJAP_CUR=\"" + ssid + "\",\"" + passwd + "\""
+    	serial.writeString(printT + "\u000D" + "\u000A")
+		bAP_Connected = waitResponse()
+    	basic.pause(4000)
     }
 
 
